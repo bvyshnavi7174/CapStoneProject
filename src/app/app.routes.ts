bad8823c26d3ac1f84844b4a1408c2aae488a9f1
bvyshnavi7174 <bvyshnavi7174@gmail.com>
2024-09-01 20:11:13 +0530

    change in code for code Maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { BuyComponent } from './components/buy/buy.component';
-import { HomeComponent } from './components/home/home.component';
 import { SellComponent } from './components/sell/sell.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { AccountComponent } from './components/account/account.component';
